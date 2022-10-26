b2e408806df26001efc9916d5c4e23789bbf0899
Parham2002 <parhamm2002@gmail.com>
2022-10-26 14:13:04 +0100

    populated the course class

--- courseology-backend/src/main/java/com/example/course/Course.java
+++ courseology-backend/src/main/java/com/example/course/Course.java
@@ -1,4 +1,40 @@
 package com.example.course;
 
+import java.time.LocalDate;
+
 public class Course {
+
+    private long id;
+    private String course;
+    private LocalDate dateCreated = LocalDate.now();
+
+    public Course(long id, String course, LocalDate dateCreated) {
+        this.id = id;
+        this.course = course;
+        this.dateCreated = dateCreated;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
 }
