d6c7e8a91f539ac11a60eb90a2e3f073cebbf0d6
Parham2002 <parhamm2002@gmail.com>
2022-10-31 14:08:47 +0000

    made adjustments to button component

--- courseology-frontend/src/Pages/Home/Home.jsx
+++ courseology-frontend/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import "./Home.scss";
+import Button from "../../Components/Button/Button"
 
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      this is home
+    <div className="home-container">
+      <h1>Welcome to Courseology!</h1>
+      <Button name="Take a look at Courses!" link="/courses"/>
     </div>
   )
 }
